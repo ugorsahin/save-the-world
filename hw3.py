cb875e4241682687819d3e8ca0e4582f68d5cc92
Uğur Şahin <e2036564@ceng.metu.edu.tr>
2019-05-24 06:23:16 +0300

    Q-learning and value iteration

--- hw3.py
+++ hw3.py
@@ -1,6 +1,45 @@
 import time
 import re
 
+class World(object):
+	def __init__(self, nodes, q_lr, disc):
+		self.nodes = nodes
+		self.qnum = sum([1 if isinstance(v, Round) else 0 for v in self.nodes.values()])
+		self.vnum = sum([1 if isinstance(v, Star) else 0 for v in self.nodes.values()])
+		self.disc = disc
+		self.qlr = q_lr
+		self.qtable = [ [0 if self.nodes[j].det_pass.get(i) else -1  for i in range(self.qnum)] for j in range(self.qnum)]
+	
+	def run_story(self, episodes):
+		node = self.nodes[episodes[0]]
+		for num in episodes[1:]:
+			reward, new_node = node.makemove(num)
+			if new_node is node:
+				print("_\n")
+				continue
+			if new_node.num > self.qnum:
+				print("Teleporting")
+				break
+			prev_a = (1-self.qlr) * self.qtable[node.num][new_node.num]
+			self.qtable[node.num][new_node.num] = prev_a + self.qlr * ( reward + self.disc * new_node.max_reward())
+			print(self.q_table())
+			node = new_node
+
+	def q_table(self):
+		outstr = "-" * (self.qnum * 8) + "\n"
+		outstr += "|" +"|".join( ["\t|".join([str(i) + " " if i>0 else "_" for i in self.qtable[i]]) + "\t|\n" for i in range(self.qnum)] ) 
+		outstr += "-" * (self.qnum * 8) + "\n"
+		return outstr
+
+	def iter_one(self):
+		for i in self.nodes.values():
+			if isinstance(i, Star):
+				i.calc_value(self.disc)
+
+		for i in self.nodes.values():
+			if isinstance(i, Star):
+				print(i.pols())
+
 class Node(object):
 
 	def __init__(self, nodenum):
@@ -14,34 +53,122 @@ class Node(object):
 			"reward" : reward
 		}
 
-	def add_action(self, num):
-		self.actions[num] = 0
-	
 	def __str__(self):
-		outstr = f"Regular Node {self.num}: "
-		outstr += ", ".join([f"{k}, {v}" for k, v in self.det_pass.items()] ) + " "  
-		outstr += " - ".join([f"{k}" for k in self.actions] )  + "\n"
+		outstr = f"{self.__doc__} {self.num}: "
+		if len(self.det_pass):
+			outstr += ", \n".join([f"{k} : {v}" for k, v in self.det_pass.items()] ) + "\n"  
+		elif len(self.actions):
+			outstr += ", \n".join([f"{k} : {v}" for k, v in self.actions.items()] ) + "\n"  
 		return outstr
 
+
 class Round(Node):
+	"Round Node"
 	def __init__(self, **kwargs):
 		Node.__init__(self, kwargs["nodenum"])
+
+	def makemove(self, num):
+		try:
+			return self.det_pass[num]["reward"], self.det_pass[num]["ptr"]
+		except:
+			return 0, self
+
+	def max_reward(self):
+		return max([v["reward"] for v in self.det_pass.values()])
 
 class Star(Node):
+	"Star Node"
 	def __init__(self, **kwargs):
 		Node.__init__(self, kwargs["nodenum"])
+		self.vtable = {} 
+		self.best_pol = []
+		self.vscore = 0
+
+	def add_action_prob(self, **kwargs):
+		a_no = kwargs["action_num"]
+		n_no = kwargs["nodenum"]
+		self.vtable[a_no] = 0
+
+		self.actions[a_no][n_no] = {
+			"r": kwargs["reward"],
+			"prob" : kwargs["prob"],
+			"ptr" : kwargs["ptr"]
+		}
+
+	def add_action(self, num):
+		self.actions[num] = {}
+
+	def calc_value(self, gamma, times=0):
+		scores = {}
+
+		for ac_no, node_ in self.actions.items():
+			for node_n, s_node in node_.items(): 
+				# print(s_node)
+				scores[node_n] = s_node["prob"] * (s_node["r"] + gamma * s_node["ptr"].vscore)
+
+
+		# print("-")
+		# print(scores)
+		self.vscore = mx = max(scores.values())
+		# print(self.vscore)
+		self.best_pol = []
+		for k, v in scores.items():
+			if v == mx:
+				self.best_pol.append(k)
+
+		print(scores)
+
+	def pols(self):
+		outstr = f"{self.num} " + ",".join([str(i) for i in self.best_pol])
+		return outstr
 
 class Teleport(Node):
+	"Teleport Node"
 	def __init__(self, **kwargs):
 		Node.__init__(self, kwargs["nodenum"])
+		self.vtable = {} 
+		self.best_pol = []
+		self.vscore = 0
+
+	def add_action(self, num):
+		self.actions[num] = {}
+
+	def add_action_prob(self, **kwargs):
+		a_no = kwargs["action_num"]
+		n_no = kwargs["nodenum"]
+
+		self.actions[a_no][n_no] = {
+			"r": kwargs["reward"],
+			"prob" : kwargs["prob"],
+			"ptr" : kwargs["ptr"]
+		}
+	def calc_value(self, gamma, times=0):
+		scores = {}
+
+		for ac_no, node_ in self.actions.items():
+			for node_n, s_node in node_.items(): 
+				# print(s_node)
+				scores[node_n] = s_node["prob"] * (s_node["r"] + gamma * s_node["ptr"].vscore)
+
+		self.vscore = mx = max(scores)
+
+		self.best_pol = []
+		for i in scores:
+			if i == mx:
+				self.best_pol.append(i)
+
+		print(scores)
 
 class Vortex(Node):
+	"Vortex Node"
 	def __init__(self, **kwargs):
 		Node.__init__(self, kwargs["nodenum"])
 
 class Goal(Node):
+	"Goal Node"
 	def __init__(self, **kwargs):
 		Node.__init__(self, kwargs["nodenum"])
+		self.vscore = 0
 
 def nodemaker(raw_nodes):
 	d_types = {
@@ -63,10 +190,18 @@ def det_edge_parser(nodes, raw_edges):
 	for f_node, e_node, reward in raw_edges:
 		nodes[f_node].add_det_pass(nodes[e_node], reward)
 
-def pos_act_parser(nodes, raw_act_table):
-	for n_id, ac_id in raw_act_table:
+def pos_act_parser(nodes, raw_act_trs):
+	for n_id, ac_id in raw_act_trs:
 		for i in ac_id:
 			nodes[n_id].add_action(i)
+
+def undet_act_parser(nodes, raw_act_table):
+	for a_no, node_ in raw_act_table.items():
+		for node_num, pos_nodes in node_.items():
+			for k, prob in pos_nodes.items():
+				if k == "r":
+					continue
+				nodes[node_num].add_action_prob(action_num=a_no, nodenum=k, prob=prob, reward=pos_nodes["r"], ptr=nodes[k])
 
 def action_parser(inpfunc):
 	long_ = []	
@@ -113,14 +248,14 @@ def dict_printer(dicti, l=0):
 
 def parseinput(inpfunc):
 	wd = {}
-	nodep = lambda x: (int(x[0]), x[1], x[2])
+	nodep = lambda x: (x[0], x[1], x[2])
 	actiondivider = lambda x: (x[0], x[1:])
 
 	wd["nodenames"] = list(inpfunc().strip())
 	wd["alpha"], wd["gamma"] = [float(i) for i in inpfunc().strip().split()]
 
 	wd["total_transition"] = int(inpfunc().strip())
-	wd["transitions"] = [ nodep([float(i) for i in inpfunc().strip().split()]) for _ in range(wd["total_transition"]) ]
+	wd["transitions"] = [ nodep([int(i) for i in inpfunc().strip().split()]) for _ in range(wd["total_transition"]) ]
 
 	wd["total_actions"] = int(inpfunc().strip())
 	wd["actions"] = [  actiondivider([int(x) for x in inpfunc().strip().split()]) for _ in range(wd["total_actions"]) ]
@@ -131,14 +266,39 @@ def parseinput(inpfunc):
 
 	return wd
 
-if __name__ == "__main__":
+def do():
+# if __name__ == "__main__":
 	filename = "input.inp"
 
+	global file_o, ctr
 	file_o = open(filename).readlines()
 	ctr = 0
 	wd = parseinput(fake_inp)
-	# dict_printer(wd)
 	nodes = nodemaker(wd["nodenames"])
 	det_edge_parser(nodes, wd["transitions"])
 	pos_act_parser(nodes, wd["actions"])
+	undet_act_parser(nodes, wd["action_table"])
 	dict_printer(nodes)
+	world = World(nodes, wd["alpha"], wd["gamma"])
+
+	while True:
+		try:
+			inp = input()
+			if inp == "$":
+				break
+		except EOFError:
+			break
+		ep_list = [int(i) for i in inp.split()]
+		world.run_story(ep_list)
+
+	while True:
+		try:
+			inp = input()
+		except EOFError:
+			break
+		if inp == "c":
+			world.iter_one()
+
+st = time.time()
+do()
+print(time.time() - st)
